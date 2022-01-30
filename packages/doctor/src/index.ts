bb5062b9a85a85072e347e9a447988b2c727e736
minhhn3 <minhhn3@vng.com.vn>
2022-01-30 17:19:24 +0700

    feat: add test with router

--- packages/doctor/src/index.ts
+++ packages/doctor/src/index.ts
@@ -1,2 +1,5 @@
-require('module-alias/register')
-export * from "@/server";
+require('module-alias/register');
+import { DoctorService } from '@/server';
+
+const service = new DoctorService();
+service.startServer();
